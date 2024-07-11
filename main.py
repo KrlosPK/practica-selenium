50db659ec9c50fdeaf7190a2dc2b7df00b4ec583
KrlosPK <centille095@gmail.com>
2024-07-11 10:24:41 -0500

    Add comments

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import pandas as pd
-from jinja2 import Environment, FileSystemLoader
 import pdfkit
+from jinja2 import Environment, FileSystemLoader
 from motivosDespido import get_motivo_despido
 
+# Configuraciones para generar el PDF
 path_to_wkesohpta = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
-
 config = pdfkit.configuration(wkhtmltopdf=path_to_wkesohpta)
 
-# 1. Leer la tabla de datos
+# * 1. Leer la tabla de datos
 firedEmployees = pd.read_csv("./empleados.csv", encoding="utf-8")
 
+# Configuraciones para obtener la plantilla HTML
 env = Environment(loader=FileSystemLoader(".", encoding="utf-8"))
 template = env.get_template("./template.html")
 
-# 2. Rellenar la plantilla de la carta de despido
+# * 2. Rellenar la plantilla de la carta de despido
 for index, row in firedEmployees.iterrows():
 
+    # Generamos las variables para agregar a la plantilla
     carta_despido = template.render(
         nombre=row["nombre"],
         apellidos=row["apellidos"],
@@ -28,6 +30,7 @@ for index, row in firedEmployees.iterrows():
         jefe_inmediato=row["jefe inmediato"],
     )
 
+    # Generamos las cartas de despido en HTML con base en las variables del CSV
     with open(
         f'./letters/carta_despido_{row["nombre"]}_{row["apellidos"]}.html',
         "w",
@@ -35,12 +38,11 @@ for index, row in firedEmployees.iterrows():
     ) as f:
         f.write(carta_despido)
 
+    # * 3. Generar el PDF de la carta
     pdfkit.from_file(
         f'./letters/carta_despido_{row["nombre"]}_{row["apellidos"]}.html',
         output_path=f'./letters_pdf/carta_despido_{row["nombre"]}_{row["apellidos"]}.pdf',
         configuration=config,
     )
-    # 3. Generar el PDF de la carta
 
-
-# 4. Enviar el correo electrónico
+# * 4. Enviar el correo electrónico
